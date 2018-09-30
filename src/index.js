95ca950ac0d5aeda20d4a22ee8d94577f3ffc574
Natalia Kazakevich <Natallia_Kazakevich@mail.ru>
2018-09-30 23:32:40 +0300

    task 7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+let counter = 0;
+let zerosCounter = 0;
+for (let i = 5 ; i <= number; i*=5) {
+  counter = Math.floor(number / i);
+  zerosCounter += counter;
+}
+return zerosCounter;
 }
